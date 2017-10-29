c7d84dde47f3142e8fc277a08e0365eab97d83d0
davidginesy <david.ginesy@gmail.com>
2017-10-28 21:02:26 -0400

    auth

--- MyEvents/MyEvents/app/src/main/java/com/example/user/myevents/AuthActivity.java
+++ MyEvents/MyEvents/app/src/main/java/com/example/user/myevents/AuthActivity.java
@@ -4,10 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -16,18 +13,21 @@ import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Arrays;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class AuthActivity extends AppCompatActivity implements View.OnClickListener {
     private static final int RC_SIGN_IN = 123;
-    FirebaseAuth auth;
+    public static final String user_mail = "com.example.user.myevnts.MESSAGE";
+    FirebaseAuth auth = FirebaseAuth.getInstance();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        auth = FirebaseAuth.getInstance();
+
         if (auth.getCurrentUser() != null) {
-            // already signed in
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
         } else {
             startActivityForResult(
                     AuthUI.getInstance()
@@ -41,29 +41,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             .build(),
                     RC_SIGN_IN);
         }
-       findViewById(R.id.Log_out_button).setOnClickListener(this);
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if( requestCode == RC_SIGN_IN){
-            if (resultCode == RESULT_OK){
-                Log.d("Auth", "loged in");
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+                return;
             }
             else{
-                //Log.d("Auth","NOT Authenticated");
+                findViewById(R.id.Log_out_button).setOnClickListener(this);
             }
         }
     }
 
     @Override
     public void onClick(View v) {
-        if(v.getId() == R.id.Log_out_button){
+        if (v.getId() == R.id.Log_out_button) {
             AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
                 @Override
                 public void onComplete(@NonNull Task<Void> task) {
-                    Log.d("Auth","User Log Out");
                     finish();
                 }
             });
